ffebbb032f43cbf007158a42bc477c16f486b53c
gbinua <92772426+gbinua@users.noreply.github.com>
2021-11-06 11:28:30 +0800

    Update new.php

--- new.php
+++ new.php
@@ -28,8 +28,8 @@ for ($x = 0;$x < $NoofDum;$x++) {
     $Dummy = readline('Input Dummy UN: ');
     array_push($dummyarray, "$Dummy",);
 }
-echo "｡☆✼★━PLEASE ENTER★✼☆｡";
-$pass = readline('PASSWORD: ');
+echo "｡☆✼★━PLEASE ENTER ★✼☆｡";
+$pass = readline('  PASSWORD: ');
 // $user = readline('DUMMY UN: ');
 // $pass = readline('DUMMY PASS: ');
 $red = "[1;31m";
